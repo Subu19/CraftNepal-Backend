095e14d6ff73eb64f94434b3a3516835da5acd92
Subu19 <subuacharya19@gmail.com>
2025-01-21 00:29:31 +0545

    Fix leaderboard of balance

--- Controllers/leaderboard/balance.js
+++ Controllers/leaderboard/balance.js
@@ -2,7 +2,7 @@ const { con } = require("../../utils/connection");
 
 exports.getBalanceLeaderboard = async (req, res, next) => {
     try {
-        con.query("SELECT player_name, money as value FROM s4_SMP_DB.eco_accounts LIMIT 100;", (err, result) => {
+        con.query("SELECT player_name, money as value FROM s4_SMP_DB.eco_accounts ORDER BY value DESC LIMIT 100;", (err, result) => {
             if (err) res.send({ err: err, data: null });
             else res.send({ err: null, data: result });
         });
